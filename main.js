9070062c2653fb0f987e815ceb7f4973de290dd3
loki <648875862@qq.com>
2019-06-13 11:10:22 +0800

    feat: 添加打包功能

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url')
 
 // 保持window对象的全局引用,避免JavaScript对象被垃圾回收时,窗口被自动关闭.
 let mainWindow
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 function createWindow () {
 //创建浏览器窗口,宽高自定义具体大小你开心就好
 mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
@@ -16,13 +16,13 @@ mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
    * 加载应用-----  electron-quick-start中默认的加载入口
 
   */
-	// mainWindow.loadURL(url.format({
-	// 	pathname: path.join(__dirname, 'index.html'),
-	// 	protocol: 'file:',
-	// 	slashes: true
-	// }))
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, './build/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
   // 加载应用----适用于 react 项目
-  mainWindow.loadURL('http://localhost:3000/');
+//   mainWindow.loadURL('http://localhost:3000/');
 
   // 打开开发者工具，默认不打开
   mainWindow.webContents.openDevTools()
@@ -36,8 +36,8 @@ mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
 // 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
 app.on('ready', () => {
 	createWindow();
-	let webview = document.querySelector('#webview');
-	console.log(webview);
+	// let webview = document.querySelector('#webview');
+	// console.log(webview);
 
 })
 
